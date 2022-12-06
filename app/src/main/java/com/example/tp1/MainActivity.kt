2c6780afbc323e3085c4f6515522d7cc0dd5e4f0
KCM <kcm4112@naver.com>
2022-12-06 14:49:53 +0900

    파이어베이스 연결

--- app/src/main/java/com/example/tp1/MainActivity.kt
+++ app/src/main/java/com/example/tp1/MainActivity.kt
@@ -4,12 +4,18 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.ktx.database
+import com.google.firebase.ktx.Firebase
 
 class MainActivity : AppCompatActivity() {
+    lateinit var database : DatabaseReference
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.main_page)
-
+        //DB연결 확인용 코드
+        database = Firebase.database.reference
+        database.child("Test01").setValue("success!")
         val firstVisit = findViewById<Button>(R.id.main_btn1)
         firstVisit.setOnClickListener{
             val intent = Intent(this, ChoosePatty::class.java)
